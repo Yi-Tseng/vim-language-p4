b77996d77ab52db4f27b8176a65b62b7a4fd6470
Yi Tseng <a86487817@gmail.com>
2016-07-27 17:05:55 +0800

    update installation script

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 mkdir -p ~/.vim/syntax
 mkdir -p ~/.vim/ftdetect
 
+curl -o- -L https://raw.githubusercontent.com/TakeshiTseng/vim-language-p4/master/ftdetect/p4.vim > ~/.vim/ftdetect/p4.vim
+curl -o- -L https://raw.githubusercontent.com/TakeshiTseng/vim-language-p4/master/syntax/p4.vim > ~/.vim/syntax/p4.vim
+
